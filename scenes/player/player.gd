2dfd19152c5a762e58c2e83f9aacb25c66bc9ee4
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-08 00:08:45 +0200

    Player, map and camera

---
+++ scenes/player/player.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const MAX_SPEED = 200
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var movement_vector = get_movement_vector()
+	var direction = movement_vector.normalized()
+	velocity = direction * MAX_SPEED
+	move_and_slide()
+
+func get_movement_vector():
+	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	return Vector2(x_movement, y_movement)
