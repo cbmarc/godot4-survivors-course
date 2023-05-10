747423359d48602d49aa5e6e07b7d8206b9056ba
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-10 23:18:17 +0200

    Upgrade UI with pause.

---
+++ scenes/ui/ability_upgrade_card.gd
@@ -0,0 +1,8 @@
+extends PanelContainer
+
+@onready var name_label: Label = $%NameLabel
+@onready var description_label: Label = $%DescriptionLabel
+
+func set_ability_upgrade(upgrade: AbilityUpgrade):
+	name_label.text = upgrade.name
+	description_label.text = upgrade.description
