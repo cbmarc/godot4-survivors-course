8c5d60ed11604c6f41dc900e37ab41b753012b2c
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-11 16:09:14 +0200

    Make ability upglrades do something.

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -3,11 +3,15 @@ extends Node
 const MAX_RANGE = 150
 
 @export var sword_ability: PackedScene
+
 var damage = 5
+var base_wait_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	base_wait_time = $Timer.wait_time
 	$Timer.timeout.connect(on_timer_timeout)
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -40,3 +44,12 @@ func on_timer_timeout():
 	
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+
+
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+		
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
