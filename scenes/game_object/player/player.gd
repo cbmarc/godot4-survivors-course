4405be47f2b22ea647c446418c17a709e947490a
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-11 21:49:04 +0200

    Player health depletes on hit.

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -3,12 +3,16 @@ extends CharacterBody2D
 const MAX_SPEED = 125
 const ACCELERATION_SMOOTHING = 25
 
-# Called when the node enters the scene tree for the first time.
+@onready var damage_interval_timer = $DamageIntervalTimer
+
+var number_colliding_bodies = 0
+
 func _ready():
-	pass # Replace with function body.
+	$CollisionArea2D.body_entered.connect(on_body_entered)
+	$CollisionArea2D.body_exited.connect(on_body_exited)
+	
+	damage_interval_timer.timeout.connect(on_damage_interval_timer_timeout)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	var movement_vector = get_movement_vector()
 	var direction = movement_vector.normalized()
@@ -17,7 +21,29 @@ func _process(delta):
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
 	move_and_slide()
 
+
 func get_movement_vector():
 	var x_movement = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var y_movement = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
 	return Vector2(x_movement, y_movement)
+
+
+func check_deal_damage():
+	if number_colliding_bodies == 0 || !damage_interval_timer.is_stopped():
+		return
+	
+	$HealthComponent.damage(1)
+	damage_interval_timer.start()
+	print($HealthComponent.current_health)
+	
+func on_body_entered(other_body: Node2D):
+	number_colliding_bodies += 1
+	check_deal_damage()
+
+
+func on_body_exited(other_body: Node2D):
+	number_colliding_bodies -=1
+
+
+func on_damage_interval_timer_timeout():
+	check_deal_damage()
