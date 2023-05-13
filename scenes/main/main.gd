644d1d8e5f49214b5ba90aa4c9b2b23b2cf741e8
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-13 15:54:54 +0200

    Defeat screen.

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -2,16 +2,11 @@ extends Node
 
 @export var end_screen_scene: PackedScene
 
-@onready var timer = $Timer
-
 func _ready():
-	timer.timeout.connect(on_timer_timeout)
-
+	$%Player.health_component.died.connect(on_player_died)
 	
-func get_time_elapsed():
-	return timer.wait_time - timer.time_left
 
-
-func on_timer_timeout():
+func on_player_died():
 	var end_screen_instance = end_screen_scene.instantiate()
 	add_child(end_screen_instance)
+	end_screen_instance.set_defeat()
