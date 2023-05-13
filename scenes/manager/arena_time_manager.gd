a071b0758c5784afb0a16e056b1d43a77c4a84f6
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-13 16:33:00 +0200

    Dificulty increments.

--- scenes/manager/arena_time_manager.gd
+++ scenes/manager/arena_time_manager.gd
@@ -1,12 +1,28 @@
 extends Node
 
+signal arena_dificulty_increased(arena_dificulty: int)
+
+const DIFICULTY_INTERVAL = 5
+
 @export var end_screen_scene: PackedScene
 
 @onready var timer = $Timer
 
+var arena_dificulty = 0
+
+
 func _ready():
 	timer.timeout.connect(on_timer_timeout)
 
+
+func _process(delta):
+	var next_time_target = timer.wait_time - ((arena_dificulty + 1) * DIFICULTY_INTERVAL)
+	
+	if timer.time_left <= next_time_target:
+		arena_dificulty += 1
+		arena_dificulty_increased.emit(arena_dificulty)
+		
+	
 	
 func get_time_elapsed():
 	return timer.wait_time - timer.time_left
--- scenes/manager/enemy_manager.gd
+++ scenes/manager/enemy_manager.gd
@@ -3,12 +3,20 @@ extends Node
 const SPAWN_RADIUS = 375
 
 @export var basic_enemy_scene: PackedScene
+@export var arena_time_manager: Node
+
+@onready var timer = $Timer
+
+var base_spawn_time = 0
 
 func _ready():
-	$Timer.timeout.connect(on_timer_timeout)
-
+	base_spawn_time = timer.wait_time
+	timer.timeout.connect(on_timer_timeout)
+	arena_time_manager.arena_dificulty_increased.connect(on_arena_dificulty_increased)
+	
 
 func on_timer_timeout():
+	timer.start()
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
@@ -21,3 +29,10 @@ func on_timer_timeout():
 	var entities_layer = get_tree().get_first_node_in_group("entities_layer")
 	entities_layer.add_child(enemy)
 	enemy.global_position = spawn_position
+
+
+func on_arena_dificulty_increased(arena_dificulty: int):
+	var time_off = (.1 / 12) * arena_dificulty
+	time_off = min(time_off, .7)
+	print (time_off)
+	timer.wait_time = base_spawn_time - time_off
