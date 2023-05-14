de335589fe46f211481bcb445509b97e3496110e
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-14 14:46:15 +0200

    Animate the player.

--- scenes/game_object/player/player.gd
+++ scenes/game_object/player/player.gd
@@ -7,6 +7,8 @@ const ACCELERATION_SMOOTHING = 25
 @onready var health_component = $HealthComponent
 @onready var health_bar = $HealthBar
 @onready var abilities = $Abilities
+@onready var animation_player = $AnimationPlayer
+@onready var visuals = $Visuals
 
 var number_colliding_bodies = 0
 
@@ -25,7 +27,19 @@ func _process(delta):
 	var target_velocity = direction * MAX_SPEED
 	
 	velocity = velocity.lerp(target_velocity, 1 - exp(-delta * ACCELERATION_SMOOTHING))
+	
 	move_and_slide()
+	
+	if movement_vector.x != 0 || movement_vector.y != 0:
+		animation_player.play("walk")
+	else:
+		animation_player.play("RESET")
+	
+	var move_sign = sign(movement_vector.x)
+	if move_sign == 0:
+		visuals.scale = Vector2.ONE
+	else:
+		visuals.scale = Vector2(move_sign, 1)
 
 
 func get_movement_vector():
