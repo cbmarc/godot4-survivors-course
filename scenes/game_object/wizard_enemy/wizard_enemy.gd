e91db14c938489ab828cd61f18cf86afaedf3f96
Marc Carné Bori <marc.carne@werlabs.com>
2023-05-16 22:18:10 +0200

    Animating the wizard.

--- scenes/game_object/wizard_enemy/wizard_enemy.gd
+++ scenes/game_object/wizard_enemy/wizard_enemy.gd
@@ -3,11 +3,20 @@ extends CharacterBody2D
 @onready var velocity_component = $VelocityComponent
 @onready var visuals = $Visuals
 
+var is_moving = false
+
 func _process(delta):
-	velocity_component.accelerate_to_player()
+	if is_moving:
+		velocity_component.accelerate_to_player()
+	else:
+		velocity_component.decelerate()
+		
 	velocity_component.move(self)
 	
 	var move_sign = sign(velocity)
 	if move_sign.x != 0:
-		visuals.scale = Vector2(-move_sign.x, 1)
+		visuals.scale = Vector2(move_sign.x, 1)
 	
+
+func set_is_moving(moving: bool):
+	is_moving = moving
